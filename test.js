856a66de5f13b5405318f84943d2475e56445830
Calvin Metcalf <cmetcalf@appgeo.com>
2014-10-13 15:55:49 -0400

    test

---
+++ test.js
@@ -0,0 +1,21 @@
+var test = require('tape');
+
+var encrypter = require('./');
+
+test('first', function (t) {
+  t.plan(2);
+  var password = new Buffer('super secret');
+  var obj = {
+    something: 'here',
+    array: ['is', 'here'],
+    number: 1
+  };
+  encrypter.objectEncrypter(obj, password).then(function (buf) {
+    t.ok(Buffer.isBuffer(buf), 'is a buffer');
+    return encrypter.objectDecrypter(buf, password);
+  }).then(function (o) {
+    t.deepEquals(o, obj, 'are equal');
+  }).catch(function (e) {
+    t.ok(false, e.message);
+  });
+});
